64fc6cdd87e0c9a654729d79159489f5de248bd7
DavidAOchoa <od174140@gmail.com>
2022-03-08 16:56:35 -0600

    Completed basic landing page

--- mockstore/src/app/app-routing.module.ts
+++ mockstore/src/app/app-routing.module.ts
@@ -3,11 +3,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './pages/about/about.component';
 import { ContactComponent } from './pages/contact/contact.component';
 import { HomeComponent } from './pages/home/home.component';
-import { MenuComponent } from './pages/menu/menu.component';
+import { BuyNowComponent } from './pages/buy-now/buy-now.component';
 
 const routes: Routes = [
   {path:'', component:HomeComponent},
-  {path:'menu', component:MenuComponent},
+  {path:'buy-now', component:BuyNowComponent},
   {path:'about', component:AboutComponent},
   {path:'contact', component:ContactComponent},
 ];
--- mockstore/src/app/app.module.ts
+++ mockstore/src/app/app.module.ts
@@ -6,9 +6,10 @@ import { AppComponent } from './app.component';
 import { NavbarComponent } from './sharepage/navbar/navbar.component';
 import { FooterComponent } from './sharepage/footer/footer.component';
 import { HomeComponent } from './pages/home/home.component';
-import { MenuComponent } from './pages/menu/menu.component';
+
 import { AboutComponent } from './pages/about/about.component';
 import { ContactComponent } from './pages/contact/contact.component';
+import { BuyNowComponent } from './pages/buy-now/buy-now.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,7 @@ import { ContactComponent } from './pages/contact/contact.component';
     NavbarComponent,
     FooterComponent,
     HomeComponent,
-    MenuComponent,
+    BuyNowComponent,
     AboutComponent,
     ContactComponent
   ],
